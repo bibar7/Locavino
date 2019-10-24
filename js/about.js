fa537cb9ca21f9ebe8882c97e3ffe18511385b8c
BIaggi <bibar7@gmail.com>
2019-10-23 21:16:27 -0400

    created about subpage js and tweaks for responsiveness

---
+++ js/about.js
@@ -0,0 +1,36 @@
+$(document).ready(function($) {
+
+	$('#video-banner').on('play', function(e) { 
+  $('#play-icon').removeClass('icon-on').addClass('icon-off');
+$('#pause-icon').removeClass('icon-off').addClass('icon-on');
+});
+
+$('#video-banner').on('pause', function(e) { 
+$('#pause-icon').removeClass('icon-on').addClass('icon-off');
+$('#play-icon').removeClass('icon-off').addClass('icon-on');
+});
+
+$('#video-banner').parent().click(function (e) {
+	if($(this).children('#video-banner').get(0).paused){        
+		$(this).children('#video-banner').get(0).play();   
+    $(this).children('#locavino-video-overlay').fadeOut();
+
+    		}
+	else{       
+		$(this).children('#video-banner').get(0).pause();
+    $(this).children('#locavino-video-overlay').fadeIn();
+
+    }
+});
+
+$('#video-banner').parent().mousemove(function (e) {
+    $('#locavino-video-overlay').show();
+});
+
+$('#video-banner').parent().mouseout(function (e) {
+  $('#locavino-video-overlay').hide();
+});
+
+});
+
+	
